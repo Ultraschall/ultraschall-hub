d0352fddec0c12fc71b5dd50c2cfc2a1be52479b
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-12-18 15:54:17 +0100

    update debug helper

--- debug_coreaudio.sh
+++ debug_coreaudio.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-sudo cp -rf /Users/danlin/Library/Developer/Xcode/DerivedData/UltraschallHub-djhwxqlajhmvaogxdqcnwlxvxmby/Build/Products/Debug/UltraschallHub.driver /Library/Audio/Plug-Ins/HAL/
-sudo killall coreaudiod
+xcodebuild -project "Driver/Source/UltraschallHub/UltraschallHub.xcodeproj" -configuration Debug -showBuildSettings | sed '1d;s/^ *//;s/"/\\"/g;s/ = \(.*\)/="\1"/;s/ = /=/;s/UID.*//' > xcodebuild-env.tmp 
+source xcodebuild-env.tmp
+rm xcodebuild-env.tmp
+sudo cp -rfv $TARGET_BUILD_DIR/$TARGET_NAME.driver /Library/Audio/Plug-Ins/HAL/
+sudo launchctl unload /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
+sudo launchctl load /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
