8ec59a519d2cd702100a836411a3a7295f7e75d1
Daniel Lindenfelser <daniel@lindenfelser.de>
2014-12-20 12:42:15 +0100

    remove old code

--- Driver/debug_coreaudio.sh
+++ Driver/debug_coreaudio.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xcodebuild -project "Driver/Source/UltraschallHub/UltraschallHub.xcodeproj" -configuration Debug -showBuildSettings | sed '1d;s/^ *//;s/"/\\"/g;s/ = \(.*\)/="\1"/;s/ = /=/;s/UID.*//' > xcodebuild-env.tmp 
+xcodebuild -project "UltraschallHub.xcodeproj" -configuration Debug -showBuildSettings | sed '1d;s/^ *//;s/"/\\"/g;s/ = \(.*\)/="\1"/;s/ = /=/;s/UID.*//' > xcodebuild-env.tmp
 source xcodebuild-env.tmp
 rm xcodebuild-env.tmp
 sudo cp -rfv $TARGET_BUILD_DIR/$TARGET_NAME.driver /Library/Audio/Plug-Ins/HAL/
