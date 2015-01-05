ee42a097c3f161a7eb1c4559b657e89d8ad06f45
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-01-05 17:57:37 +0100

    cleanup debug script

--- Plug-in/debug_coreaudio.sh
+++ Plug-in/debug_coreaudio.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-xcodebuild -project "UltraschallHub.xcodeproj" -configuration Debug clean
-xcodebuild -project "UltraschallHub.xcodeproj" -configuration Debug build
-
 sudo launchctl unload /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
 if [ -d /Library/Audio/Plug-Ins/HAL/UltraschallHub.driver ]; then
-	rm -rf /Library/Audio/Plug-Ins/HAL/UltraschallHub.driver
+	sudo rm -rf /Library/Audio/Plug-Ins/HAL/UltraschallHub.driver
 fi
-sudo cp -rfv ./build/Debug/$TARGET_NAME.driver /Library/Audio/Plug-Ins/HAL/
+sudo cp -rfv ./build/Debug/UltraschallHub.driver /Library/Audio/Plug-Ins/HAL/
 sudo launchctl load /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
