c6d78a98bddabc3fc1be718f79d9f29a2d923b0e
Daniel Lindenfelser <daniel@lindenfelser.de>
2015-04-23 22:50:26 +0200

    update installer

--- Installer/build_release.sh
+++ Installer/build_release.sh
@@ -10,8 +10,8 @@ fi
 mkdir ./Payload
 
 echo "Build Core Audio Server Plug-in"
-xcodebuild -project "../Plug-in/UltraschallHubDriver.xcodeproj" -configuration $CONFIGURATION clean
-xcodebuild -project "../Plug-in/UltraschallHubDriver.xcodeproj" -configuration $CONFIGURATION build
+xcodebuild -project "../Plug-in/UltraschallHubDriver.xcodeproj" -configuration $CONFIGURATION clean > /dev/null
+xcodebuild -project "../Plug-in/UltraschallHubDriver.xcodeproj" -configuration $CONFIGURATION build > /dev/null
 
 echo "Create Core Audio Server Plug-in Package"
 if [ -d ../Plug-in/Payload ]; then
@@ -19,19 +19,19 @@ if [ -d ../Plug-in/Payload ]; then
 fi
 mkdir ../Plug-in/Payload
 cp -rf ../Plug-in/build/$CONFIGURATION/*.driver ../Plug-in/Payload/
-pkgbuild --root ../Plug-in/Payload --identifier fm.ultraschall.UltraschallHub-PlugIn --scripts ../Plug-in/Scripts --install-location /Library/Audio/Plug-ins/HAL ./Payload/UltraschallHub-PlugIn.$VERSION.pkg
+pkgbuild --root ../Plug-in/Payload --identifier fm.ultraschall.UltraschallHubDriver --scripts ../Plug-in/Scripts --install-location /Library/Audio/Plug-ins/HAL ./Payload/UltraschallHubDriver.$VERSION.pkg > /dev/null
 rm -rf ../Plug-in/Payload
 
 echo "Create Installer"
-productbuild --distribution UltraschallHub.plist --package-path Payload --resources Resources ./Package/UltraschallHub.$VERSION.pkg
+productbuild --distribution UltraschallHub.plist --package-path Payload --resources Resources ./Package/UltraschallHub.$VERSION.pkg > /dev/null
 
 echo "Create Image"
 if [ -f UltraschallHub-$VERSION.dmg.gz ]; then
 	rm -f UltraschallHub-$VERSION.dmg.gz
 fi
 
-hdiutil create -srcfolder ./Package -volname "UltraschallHub-$VERSION" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 50m pack.temp.dmg
-device=$(hdiutil attach -readwrite -noverify -noautoopen "pack.temp.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+hdiutil create -srcfolder ./Package -volname "UltraschallHub-$VERSION" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 50m pack.temp.dmg > /dev/null
+device=$(hdiutil attach -readwrite -noverify -noautoopen "pack.temp.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}') > /dev/null
 
 echo '
    tell application "Finder"
@@ -51,13 +51,13 @@ echo '
            close
      end tell
    end tell
-' | osascript
+' | osascript > /dev/null
 
 chmod -Rf go-w /Volumes/UltraschallHub-$VERSION
 sync
 sync
-hdiutil detach ${device}
-hdiutil convert pack.temp.dmg -format UDZO -imagekey zlib-level=9 -o ./UltraschallHub-$VERSION.dmg
+hdiutil detach ${device} > /dev/null
+hdiutil convert pack.temp.dmg -format UDZO -imagekey zlib-level=9 -o ./UltraschallHub-$VERSION.dmg > /dev/null
 rm -f pack.temp.dmg 
 
 gzip ./UltraschallHub-$VERSION.dmg
